b6484c4b310385ce9568d04647056afce6750e9e
krishna kant rathore <rathor.kk@gmail.com>
2020-05-01 17:47:11 +0530

    Doubly Linked list skeleton.

--- FreeCodeCamp/FreeCodeCamp/DoublyLL.h
+++ FreeCodeCamp/FreeCodeCamp/DoublyLL.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "NodeLL.h"
 
 /* Steps: 
 	1) Node Creation
@@ -9,9 +10,28 @@ template <typename T>
 class DoublyLL
 {	
 private:
-
+	T *head;
+	T *tail; 
+	int size;
 public:
 	DoublyLL();
+	T* getHead();
+	T* getTail();
+
+	int getSize();
+
+	void addNodeAsHead(T* node);
+	void addNodeAsTail(T* node);
+	void push(T* new_node);
+	T* pop();
+	T* peek();
+	void enqueue(T* new_node);
+	T* dequeue();
+	T* get(int index);
+	void printList();
+	void printListBackwards();
+	~MyList();
+
 	~DoublyLL();
 	void clear() {
 
